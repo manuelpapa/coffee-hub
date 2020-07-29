11c13e1f3f5c2d74a2718db3aaba0185c1e36829
manuelpapa <manulito@de-vio.com>
2020-07-29 16:40:12 +0200

    Add js and css

--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -1,14 +1,36 @@
-import { createButton } from "../../components/button/button";
+import "./details.css";
+import { createElement } from "../../utils/elements";
+import { coffeeSelector } from "../../components/quantitySelector/quantitySelector.stories";
+import macchiatoSrc from "../../assets/macchiato.svg";
 
-export default { title: "Pages/Details" };
+export default {
+  title: "Pages/Details",
+};
 
 export const basic = () => {
-  const main = document.createElement("main");
+  const main = createElement("main", {
+    className: "details",
+  });
+  //creations
+  const header = createElement("div", {
+    className: "details__header",
+  });
 
-  // const quantitySelector = createQuantitySelector();
-  // main.append(quantitySelector);
-  const button = createButton("Add to cart");
-  main.append(button);
+  const coffeeTitle = createElement("h2", {
+    className: "details__title",
+    innerText: "Macchiato",
+  });
+  const macchiatoImage = createElement("img", {
+    src: macchiatoSrc,
+    alt: "picture of a macchiato in venti with sugar",
+  });
 
+  const quantitySelector = coffeeSelector();
+
+  //relationship
+  main.append(header);
+  header.append(coffeeTitle);
+  main.append(quantitySelector);
+  header.append(macchiatoImage);
   return main;
 };
