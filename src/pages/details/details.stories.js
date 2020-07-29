b925b8b18e8181879a517d9e0feb6036f289b773
manuelpapa <manulito@de-vio.com>
2020-07-29 21:02:32 +0200

    Add stories.js

--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -1,36 +1,45 @@
-import "./details.css";
-import { createElement } from "../../utils/elements";
-import { coffeeSelector } from "../../components/quantitySelector/quantitySelector.stories";
-import macchiatoSrc from "../../assets/macchiato.svg";
+// import "./details.css";
+// import { createElement } from "../../utils/elements";
+// import { coffeeSelector } from "../../components/quantitySelector/quantitySelector.stories";
+// import macchiatoSrc from "../../assets/macchiato.svg";
 
-export default {
-  title: "Pages/Details",
-};
+// export default {
+//   title: "Pages/Details",
+// };
+
+// export const basic = () => {
+//   const main = createElement("main", {
+//     className: "details",
+//   });
+//   //creations
+//   const header = createElement("div", {
+//     className: "details__header",
+//   });
+
+//   const coffeeTitle = createElement("h2", {
+//     className: "details__title",
+//     innerText: "Macchiato",
+//   });
+//   const macchiatoImage = createElement("img", {
+//     src: macchiatoSrc,
+//     alt: "picture of a macchiato in venti with sugar",
+//   });
+
+//   const quantitySelector = coffeeSelector();
+
+//   //relationship
+//   main.append(header);
+//   main.append(coffeeTitle);
+//   main.append(quantitySelector);
+//   header.append(macchiatoImage);
+//   return main;
+// };
+
+import { createDetailsPage } from "./details";
+
+export default { title: "Pages/Details" };
 
 export const basic = () => {
-  const main = createElement("main", {
-    className: "details",
-  });
-  //creations
-  const header = createElement("div", {
-    className: "details__header",
-  });
-
-  const coffeeTitle = createElement("h2", {
-    className: "details__title",
-    innerText: "Macchiato",
-  });
-  const macchiatoImage = createElement("img", {
-    src: macchiatoSrc,
-    alt: "picture of a macchiato in venti with sugar",
-  });
-
-  const quantitySelector = coffeeSelector();
-
-  //relationship
-  main.append(header);
-  header.append(coffeeTitle);
-  main.append(quantitySelector);
-  header.append(macchiatoImage);
-  return main;
+  const detailsPage = createDetailsPage();
+  return detailsPage;
 };
